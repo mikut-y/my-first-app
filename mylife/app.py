021adf11e68e1b14428daad2586ec30b7f634562
mikut-y <mikuty2525@gmail.com>
2026-03-12 13:09:56 +0900

    update code

--- mylife/app.py
+++ mylife/app.py
@@ -26,5 +26,20 @@ def handle_message(event):
         TextSendMessage(text=reply_message)
     )
 
+@app.route("/callback",methods=['POST'])
+def callback():
+    signature=request.headers['X-Line-Signature']
+    body=request.get_data(as_text=True)
+
+    try:
+        handler.handle(body,signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
 if __name__=="__main__":
-    app.run
+    import os
+    port=int(os.getenv("PORT",10000))
+    app.run("host=0.0.0.0". port=port)
